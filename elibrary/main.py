82b9a082ca7d6fa52af2ec02467ad5d6927d83c2
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-10-01 16:18:40 +0300

    move: app.py to main.py

--- elibrary/main.py
+++ elibrary/main.py
@@ -1,8 +1,7 @@
-#! /usr/bin/env python
-
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
@@ -12,14 +11,17 @@ def hello():
 <li> <a href="/other">Other</a>
 </ul>"""
 
+
 @app.route("/other")
 def other():
     return """<h1>eLibrary</h1>
 <h2>Why you would ever need another author?</h2>
 <a href="/">Yes, I apologize</a>"""
 
+
 def run():
     app.run(host="0.0.0.0", port=5000)
+
 
 if __name__ == "__main__":
     run()
