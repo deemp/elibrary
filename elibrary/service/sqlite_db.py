b61ad2c040e814aacebc08a8d267a93f3aa30fe9
Danila Danko <d.danko@innopolis.university>
2023-10-02 01:25:12 +0300

    fix: use contexts and higher-order-functions

--- elibrary/service/sqlite_db.py
+++ elibrary/service/sqlite_db.py
@@ -1,34 +1,36 @@
 import sqlite3
 import json
+from contextlib import closing
+import io
+
+
+def with_db(db, func):
+    with closing(sqlite3.connect(db)) as conn:
+        with closing(conn.cursor()) as cursor:
+            return func(conn, cursor)
 
 
 def read_script():
     with open("setup.sql") as f:
         sql = f.read()
-
     return sql
 
 
-def setup_db():
-    conn = sqlite3.connect("test.db")
-
-    db_cursor = conn.cursor()
-    db_cursor.executescript(read_script())
+def setup_db(conn, cursor):
+    cursor.executescript(read_script())
     conn.commit()
-
     print("database set up successfully")
-    conn.close()
 
 
-def test_get_all_books():
-    conn = sqlite3.connect("test.db")
-
-    db_cursor = conn.cursor()
-    db_cursor.execute("select * from books")
-    books = db_cursor.fetchall()
-    conn.close()
-
+def test_get_all_books(conn, cursor):
+    cursor.execute("select * from books")
+    books = cursor.fetchall()
     return json.dumps(books)
+
+
+def reset_db(conn, cursor):
+    cursor.execute("drop table if exists books")
+    print("removed the table `books` if it exists")
 
 
 def reset_database():
@@ -41,4 +43,7 @@ def reset_database():
 
 
 if __name__ == "__main__":
-    setup_db()
+    db = "test.db"
+    def run(f): with_db(db, f)
+    
+    run(setup_db)
