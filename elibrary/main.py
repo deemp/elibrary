308a89580975d602c4d20786d7c6b0592637e834
Danila Danko <d.danko@innopolis.university>
2023-10-03 14:20:38 +0300

    fix: poetry run

--- elibrary/main.py
+++ elibrary/main.py
@@ -1,6 +1,9 @@
-from website import create_app
+from elibrary.website import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
+def run():
     app.run(debug=True)
+    
+if __name__ == "__main__":
+    run()
