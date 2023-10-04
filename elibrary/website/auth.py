f0887884dc67cc26fb15234ee5af7b0c72e356ef
Igor Parfenov <parfenov_2001@mail.ru>
2023-10-04 16:28:15 +0300

    [database] Basic database interaction; table and search in home page.

--- elibrary/website/auth.py
+++ elibrary/website/auth.py
@@ -4,6 +4,7 @@ auth = Blueprint("auth", __name__)
 
 @auth.route("/login", methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
-    return render_template("login.html", text="Testing", user="Tim")
+    ans = "test"
+    if request.method == 'POST':
+        ans = "Hello " + request.form['email']
+    return render_template("login.html", ans=ans)
