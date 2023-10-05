19f1c48cff131263a18872220012a2495d3cef10
Danila Danko <d.danko@innopolis.university>
2023-10-05 18:37:05 +0300

    add: nix wrapper for script that generates images

--- flake.nix
+++ flake.nix
@@ -6,23 +6,32 @@
       url = "github:edolstra/flake-compat/35bb57c0c8d8b62bbfd284272c928ceb64ddbde9";
       flake = false;
     };
-    devshell.url = "github:numtide/devshell/cd4e2fda3150dd2f689caeac07b7f47df5197c31";
+    flakes.url = "github:deemp/flakes/93dacca29b38865b76ef5e8c4c5c81df426cf5e8";
   };
-  outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = inputs.nixpkgs.legacyPackages.${system};
-      devshell = inputs.devshell.legacyPackages.${system};
-      devShells.default = devshell.mkShell {
-        commands = map (x: { package = x; }) [
-          pkgs.curl
-          pkgs.jq
-          pkgs.sqlite
-          pkgs.poetry
-          pkgs.nodejs
-        ];
-      };
-    in
+  outputs = inputs: inputs.flakes.makeFlake
     {
-      inherit devShells;
-    });
+      inputs = { inherit (inputs.flakes.all) devshell drv-tools nixpkgs; };
+      perSystem = { inputs, system }:
+        let
+          pkgs = inputs.nixpkgs.legacyPackages.${system};
+          inherit (inputs.drv-tools.lib.${system}) getExe mkShellApps;
+          inherit (inputs.devshell.lib.${system}) mkShell;
+          packages = mkShellApps {
+            page-images.text = ''${getExe pkgs.poetry} run page-images -p ${pkgs.poppler_utils}/bin'';
+          };
+          devShells.default = mkShell {
+            commands = map (x: { package = x; }) [
+              pkgs.curl
+              pkgs.jq
+              pkgs.sqlite
+              pkgs.poetry
+              pkgs.nodejs
+              pkgs.poppler_utils
+            ];
+          };
+        in
+        {
+          inherit packages devShells;
+        };
+    };
 }
