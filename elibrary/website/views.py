6b70bd9b8397b3fbd9dca7f2a9b49d3bbbbcd825
Kim Daniel Fom <k.fom@innopolis.university>
2023-10-05 18:01:43 +0300

    Clean up vulnerable sql queries and replace with sql alchemy

--- elibrary/website/views.py
+++ elibrary/website/views.py
@@ -1,26 +1,17 @@
 from flask import Blueprint, render_template, request
-import sqlite3
+from .models import Book
 
 views = Blueprint('views', __name__)
 
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
 
 @views.route('/', methods=['GET', 'POST'])
 def home():
-    conn = get_db_connection()
     books = []
 
-    topics = conn.execute('select distinct lc from books').fetchall()
+    topics = Book.query.distinct().all()
 
     if request.method == 'POST':
-        print(request.form.get('topic'))
-        books = conn.execute('select * from books where lc = \'' + request.form.get('topic') + '\'').fetchall()
+        books = Book.query.filter_by(lc=request.form.get('topic')).all()
     else:
-        books = conn.execute('select * from books').fetchall()
-    
-    conn.close()
-
+        books = Book.query.all()
     return render_template("home.html", books=books, topics=topics)
