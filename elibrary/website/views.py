6e5c6d63e005ddbc52d8fd3c2e4280042f78d744
Kim Daniel Fom <k.fom@innopolis.university>
2023-10-05 18:36:23 +0300

    Move search logic to new search endpoint

--- elibrary/website/views.py
+++ elibrary/website/views.py
@@ -6,12 +6,18 @@ views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST'])
 def home():
+    return render_template("home.html")
+
+
+@views.route('/search', methods=['GET', 'POST'])
+def search():
     books = []
 
     topics = Book.query.distinct().all()
 
     if request.method == 'POST':
         books = Book.query.filter_by(lc=request.form.get('topic')).all()
-    else:
-        books = Book.query.all()
-    return render_template("home.html", books=books, topics=topics)
+        return render_template("search.html", books=books, topics=topics)
+
+    books = Book.query.all()
+    return render_template("search.html", books=books, topics=topics)
