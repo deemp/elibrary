633646ae6b2f2a41cd0f9b87959191ef1ebd1277
Igor Parfenov <parfenov_2001@mail.ru>
2023-10-11 11:55:37 +0300

    [bookreader] Change /bookreader endpoint into /bookreader/<int> and connect it to /book

--- elibrary/website/bookreader.py
+++ elibrary/website/bookreader.py
@@ -4,7 +4,8 @@ from flask_login import current_user, login_required
 bookreader = Blueprint('bookreader', __name__)
 
 
-@bookreader.route('/bookreader')
+@bookreader.route('/bookreader/<int:book_id>')
 @login_required
-def read():
-    return render_template("bookreader.html", user=current_user)
+def read(book_id: int):
+    book_id = str(book_id).rjust(6, '0')
+    return render_template("bookreader.html", user=current_user, book_id=book_id)
