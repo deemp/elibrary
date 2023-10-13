e23e43aa63ab99099a1e5c8a0d44f8f29bd4a48c
Kim Fom <452-k.fom@users.noreply.gitlab.pg.innopolis.university>
2023-10-13 16:32:09 +0300

    Move book component to separate file

---
+++ front/src/components/Book.tsx
@@ -0,0 +1,10 @@
+export function Book({ bookId }: { bookId: number }) {
+  return (
+    <iframe
+      id="reader"
+      src={`/pdfjs/web/viewer.html?file=${
+        import.meta.env.VITE_API_PREFIX
+      }/book/${bookId}/file`}
+    />
+  );
+}
