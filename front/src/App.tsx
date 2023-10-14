3c99c44f0f93258f16990241326cdb08bc0b39a9
Danila Danko <d.danko@innopolis.university>
2023-10-14 20:18:11 +0300

    fix: layout

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,6 @@
 import { Book } from "./models/book";
 import "./App.css";
-import { Form } from "react-bootstrap";
-import InputGroup from "react-bootstrap/InputGroup";
+import { Col, Row } from "react-bootstrap";
 import { Autocomplete, TextField } from "@mui/material";
 import { Table } from "./components/Table";
 import React, { useState, useEffect } from "react";
@@ -24,10 +23,10 @@ export function Search() {
   ];
   const [books, setBooks] = useState<Book[]>([]);
   const [filters, setFilters] = useState<Filter[]>([]);
-  const [filter, setFilter] = useState<string>();
+  const [filter, setFilter] = useState<string>("");
   const [searchInput, setSearchInput] = useState<string>("");
 
-  const url = "http://127.0.0.1:5000/search";
+  const url = `${import.meta.env.VITE_API_PREFIX}/search`;
 
   useEffect(() => {
     fetch(url, {
@@ -50,7 +49,7 @@ export function Search() {
       .then((r: Book[]) => {
         setBooks([...r]);
       });
-  }, []);
+  }, [url]);
 
   const handleSearch = (e: React.FormEvent) => {
     e.preventDefault();
@@ -68,59 +67,63 @@ export function Search() {
       });
   };
 
+  function SearchField({ isLeft, label, id, options, colWidth }: {
+    isLeft: boolean, label: string, id: string, options: string[], colWidth?: number
+  }) {
+    return (
+      <Col className={`p${isLeft ? 'e' : 's'}-0 ${colWidth ? `col-${colWidth}` : ""}`}>
+        <Autocomplete
+          disablePortal
+          id={id}
+          options={options}
+          renderInput={(params) =>
+            <TextField {...params} label={label}
+              sx={{
+                "& .MuiOutlinedInput-root": {
+                  borderRadius: isLeft ? "6px 0px 0px 6px" : "0px 6px 6px 0px",
+                },
+              }}
+            />
+          }
+          className="mb-3"
+        />
+      </Col>
+    )
+  }
+
+  const bisac = ["Python", "C++", "Pascal", "Java", "C#"]
+  const lc = ["OOP", "FP", "PP", "TDD"]
+  const filtersStrings = filters.map(toString)
+  const filtersInputs = ["input1", "input2"]
+
   return (
     <>
-      <div className="container">
-        <form method="POST">
-          <div className="mt-5">
-            <div className="md-col-5">
-              <InputGroup className="mb-3 md-col-2">
-                <Form.Select
-                  onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
-                    e.preventDefault();
-                    setFilter(e.target.value);
-                  }}
-                >
-                  <option value="">-- Select a filter --</option>
-                  {filters.map((filter) => (
-                    <option value={filter.toString()}>
-                      {filter.toString()}
-                    </option>
-                  ))}
-                </Form.Select>
-                <Form.Control
-                  placeholder="Type something"
-                  name={searchInput}
-                  onChange={(e) => setSearchInput(e.target.value)}
-                />
-              </InputGroup>
-              <Autocomplete
-                disablePortal
-                id="combo-box-demo"
-                options={["Python", "C++", "Pascal", "Java", "C#"]}
-                sx={{ width: 300 }}
-                renderInput={(params) => (
-                  <TextField {...params} label="bisac" />
-                )}
-                className="mb-3"
+      <Row className="justify-content-center">
+        <Col className="col-10">
+          <Row className="mt-5">
+            <SearchField isLeft={true} label={"bisac"} id={"bisac"} options={bisac}></SearchField>
+            <SearchField isLeft={false} label={"lc"} id={"lc"} options={lc}></SearchField>
+          </Row>
+          <Row>
+            <SearchField isLeft={true} label={"Filter"} id={"filter"} options={filtersStrings} colWidth={3}></SearchField>
+            <SearchField isLeft={false} label={"Filter input"} id={"filter-input"} options={filtersInputs}></SearchField>
+          </Row>
+          <Row className="mb-3">
+            <Col>
+              <SearchButton
+                setBooks={setBooks}
+                url={url}
+                handleOnClick={handleSearch}
               />
-              <Autocomplete
-                disablePortal
-                id="combo-box-demo"
-                options={["OOP", "FP", "PP", "TDD"]}
-                sx={{ width: 300 }}
-                renderInput={(params) => <TextField {...params} label="lc" />}
-              />
-            </div>
-          </div>
-          <SearchBotton
-            setBooks={setBooks}
-            url={url}
-            handleOnClick={handleSearch}
-          />
-        </form>
-        <Table heading={heading} body={books} />
-      </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Table heading={heading} body={books} />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
     </>
   );
 }
