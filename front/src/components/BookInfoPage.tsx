a745df6b05bf1a93ef808f107c5c897a9c7c1049
Kim Fom <452-k.fom@users.noreply.gitlab.pg.innopolis.university>
2023-10-17 13:36:44 +0300

    Add book cover to book page

--- front/src/components/BookInfoPage.tsx
+++ front/src/components/BookInfoPage.tsx
@@ -1,17 +1,17 @@
 import { Link, useParams } from "react-router-dom";
 import { Base } from "./Base";
 import { NavLink } from "./NavLink";
-import { Button, Container, Grid, Typography } from "@mui/material";
+import { Box, Button, Container, Grid, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { Book } from "../models/book";
+import cover_url from "../assets/book_cover.png";
 
 export function BookInfoPage() {
   const { id } = useParams();
   if (id) {
-
     const bookId = Number.parseInt(id);
-    const url = `${import.meta.env.VITE_API_PREFIX}/search`
-    let [book, setBook] = useState<Book | undefined>()
+    const url = `${import.meta.env.VITE_API_PREFIX}/search`;
+    const [book, setBook] = useState<Book | undefined>();
 
     useEffect(() => {
       fetch(url, {
@@ -25,21 +25,25 @@ export function BookInfoPage() {
         });
     }, [setBook, url]);
 
-    function Row({ title, content }: { title: string | undefined, content: string | undefined }) {
+    function Row({
+      title,
+      content,
+    }: {
+      title: string | undefined;
+      content: string | undefined;
+    }) {
       return (
         <>
-          <Grid item xs={2}>
-            <Typography variant="h6" component="div">
-              {title}
+          <Grid item xs={4} md={2}>
+            <Typography variant="p" component="div">
+              {title}:
             </Typography>
           </Grid>
-          <Grid item xs={10}>
-            <Typography component="div">
-              {content}
-            </Typography>
+          <Grid item xs={8} md={10}>
+            <Typography component="div">{content}</Typography>
           </Grid>
         </>
-      )
+      );
     }
 
     const base = (
@@ -47,31 +51,40 @@ export function BookInfoPage() {
         title="Info"
         user={{ isAuthenticated: true }}
         content={
-          <Container maxWidth='lg'>
-            <Grid container rowSpacing={1} paddingTop={2}>
-              <Grid item xs={12}>
-                <Link to={`/book/${bookId}/read`}>
-                  <Button variant="outlined">
-                    READ
-                  </Button>
-                </Link>
+          <Container maxWidth="lg">
+            <Box sx={{ display: "flex" }}>
+              <img
+                style={{ height: "calc(100vh * 3/4)", margin: "1.5rem" }}
+                src={cover_url}
+                alt={`Image of ${book?.title}`}
+                loading="lazy"
+              />
+              <Grid container spacing={2} paddingTop={2}>
+                <Grid item xs={12}>
+                  <Link to={`/book/${bookId}/read`}>
+                    <Button variant="outlined">READ</Button>
+                  </Link>
+                </Grid>
+                <Row title="BISAC" content={`${book?.bisac}`}></Row>
+                <Row title="LC" content={`${book?.lc}`}></Row>
+                <Row title="Publisher" content={`${book?.publisher}`}></Row>
+                <Row title="Year" content={`${book?.year}`}></Row>
+                <Row title="Author(s)" content={`${book?.authors}`}></Row>
+                <Row title="Title" content={`${book?.title}`}></Row>
+                <Row
+                  title="Imprint publisher"
+                  content={`${book?.imprint_publisher}`}
+                ></Row>
+                <Row title="ISBN" content={`${book?.isbn}`}></Row>
+                <Row title="ESBN" content={`${book?.esbn}`}></Row>
+                <Row title="OCLC" content={`${book?.oclc}`}></Row>
+                <Row title="LCC" content={`${book?.lcc}`}></Row>
+                <Row title="Dewey" content={`${book?.dewey}`}></Row>
               </Grid>
-              <Row title="BISAC" content={`${book?.bisac}`}></Row>
-              <Row title="LC" content={`${book?.lc}`}></Row>
-              <Row title="Publisher" content={`${book?.publisher}`}></Row>
-              <Row title="Year" content={`${book?.year}`}></Row>
-              <Row title="Author(s)" content={`${book?.authors}`}></Row>
-              <Row title="Title" content={`${book?.title}`}></Row>
-              <Row title="Imprint publisher" content={`${book?.imprint_publisher}`}></Row>
-              <Row title="ISBN" content={`${book?.isbn}`}></Row>
-              <Row title="ESBN" content={`${book?.esbn}`}></Row>
-              <Row title="OCLC" content={`${book?.oclc}`}></Row>
-              <Row title="LCC" content={`${book?.lcc}`}></Row>
-              <Row title="Dewey" content={`${book?.dewey}`}></Row>
-            </Grid>
+            </Box>
           </Container>
         }
-        nav={<NavLink text={"Search"} to={'/'} id={"search"} />}
+        nav={<NavLink text={"Search"} to={"/"} id={"search"} />}
       ></Base>
     );
     return base;
