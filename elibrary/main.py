786c2a4ac149af73498da38d466a08aec06143ce
Danila Danko <d.danko@innopolis.university>
2023-10-31 04:12:35 +0300

    fix: elibrary start

--- elibrary/main.py
+++ elibrary/main.py
@@ -7,12 +7,13 @@ from .routers import book, root, search, auth
 from .routers import auth
 from starlette.middleware.sessions import SessionMiddleware
 from . import env
+from .internal.import_catalog import run
 
 
 # https://fastapi.tiangolo.com/advanced/events/
 @asynccontextmanager
 async def lifespan(_: FastAPI):
-    create_db_and_tables()
+    run()
     yield
 
 
@@ -22,13 +23,22 @@ prefix = env.PREFIX
 
 if env.ENABLE_AUTH:
     from . import auth_secrets
+
     # https://stackoverflow.com/a/73924330/11790403
     app.add_middleware(SessionMiddleware, secret_key=auth_secrets.SECRET_KEY)
     app.include_router(auth.router, prefix=prefix)
 
 # https://fastapi.tiangolo.com/tutorial/static-files/
-app.mount(f"{prefix}/static", StaticFiles(directory="elibrary/static/front/", follow_symlink=True), name="static")
-app.mount(f"{prefix}/covers", StaticFiles(directory="covers", follow_symlink=True), name="covers")
+app.mount(
+    f"{prefix}/static",
+    StaticFiles(directory="elibrary/static/front/", follow_symlink=True),
+    name="static",
+)
+app.mount(
+    f"{prefix}/covers",
+    StaticFiles(directory="covers", follow_symlink=True),
+    name="covers",
+)
 
 # https://fastapi.tiangolo.com/tutorial/bigger-applications/
 app.include_router(root.router, prefix=prefix)
