c1d30a124a0f7938cfd05ce8d3bdc8b15f405b79
Danila Danko <d.danko@innopolis.university>
2023-10-31 13:32:53 +0300

    add: parameters

--- elibrary/__init__.py
+++ elibrary/__init__.py
@@ -10,13 +10,20 @@ class Env:
     ENABLE_AUTH: bool
     PREFIX: str
     
+    DO_IMPORT_CATALOG: bool
     XLSX_PATH: str
     SHEET: str
-    
     SQL_DUMP_PATH: str
+
+    DO_EXTRACT_COVERS: bool
+    POPPLER_PATH: str
+    BOOKS_DIR: str
+    COVERS_DIR: str
 
     def __post_init__(self):
         self.ENABLE_AUTH = self.ENABLE_AUTH == 'true'
+        self.DO_IMPORT_CATALOG = self.DO_IMPORT_CATALOG == 'true'
+        self.DO_EXTRACT_COVERS = self.DO_EXTRACT_COVERS == 'true'
 
 
 env = Env(**dotenv_values(Path(__file__).parent / ".env"))
