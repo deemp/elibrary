7a5b52d7968e69de8ac89bf77d3c258949831557
Danila Danko <d.danko@innopolis.university>
2023-11-07 06:20:48 +0300

    add: telemetry

--- elibrary/main.py
+++ elibrary/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
@@ -9,6 +10,8 @@ from starlette.middleware.sessions import SessionMiddleware
 from . import env
 from .internal.import_catalog import import_catalog
 from .internal.extract_covers import extract_covers
+from .internal.otlp import PrometheusMiddleware, metrics, setting_otlp
+import logging
 
 
 # https://fastapi.tiangolo.com/advanced/events/
@@ -22,7 +25,31 @@ async def lifespan(_: FastAPI):
     yield
 
 
+
 app = FastAPI(lifespan=lifespan)
+
+
+
+APP_NAME = os.environ.get("APP_NAME", "app")
+EXPOSE_PORT = os.environ.get("EXPOSE_PORT", 8000)
+OTLP_GRPC_ENDPOINT = os.environ.get("OTLP_GRPC_ENDPOINT", "http://tempo:4317")
+
+# Setting metrics middleware
+app.add_middleware(PrometheusMiddleware, app_name=APP_NAME)
+app.add_route("/metrics", metrics)
+
+# Setting OpenTelemetry exporter
+setting_otlp(app, APP_NAME, OTLP_GRPC_ENDPOINT)
+
+
+class EndpointFilter(logging.Filter):
+    # Uvicorn endpoint access log filter
+    def filter(self, record: logging.LogRecord) -> bool:
+        return record.getMessage().find("GET /metrics") == -1
+
+# Filter out /endpoint
+logging.getLogger("uvicorn.access").addFilter(EndpointFilter())
+
 
 prefix = env.PREFIX
 
@@ -54,3 +81,4 @@ app.include_router(search.router, prefix=prefix)
 @app.api_route("/{path:path}", methods=["GET"])
 async def catch_all(path: str):
     return FileResponse("elibrary/static/front/index.html")
+
