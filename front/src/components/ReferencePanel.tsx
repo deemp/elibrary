7cf331682ca2f8ebd8ce54c9bb0a45d29c85912b
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-11-14 02:07:05 +0300

    fix: reference panel

--- front/src/components/ReferencePanel.tsx
+++ front/src/components/ReferencePanel.tsx
@@ -1,17 +1,18 @@
-import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
-import { SyntheticEvent, useState } from "react";
+import { ReactNode, SyntheticEvent, useState } from "react";
 import { Button, Grid } from "@mui/material";
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 import copy from "copy-to-clipboard";
 import { toast } from "react-toastify";
-import { CustomTabPanel } from "./CustomTabPanel";
 import { buttonPadding, fontSize } from "../models/elements";
+import React from "react";
 
-function a11yProps(index: number) {
-  return {
-    id: `reference-tab-${index}`,
-    "aria-controls": `reference-tabpanel-${index}`,
-  };
+export interface TabPanelProps {
+  children?: ReactNode;
+  index: number;
+  value: number;
 }
 
 interface ReferenceProps {
@@ -19,33 +20,54 @@ interface ReferenceProps {
   bibTexReference: string;
 }
 
+interface Reference {
+  text: string
+}
+
+function tab(value: string, reference: string, copyToClipboard: () => void) {
+  return (
+    <TabPanel value={value}>
+      <Grid container>
+        <Grid item xs>
+          {reference}
+        </Grid>
+        <Grid item display={'flex'} sx={{
+          alignItems: 'start',
+          justifyContent: { xs: 'center', sm: 'right' },
+          width: { xs: '100%', sm: '150px' }
+        }}>
+          <Button
+            sx={{
+              fontSize,
+              ...buttonPadding,
+            }}
+            variant="contained"
+            size="large"
+            disableElevation
+            onClick={copyToClipboard}
+          >
+            copy
+          </Button>
+        </Grid>
+      </Grid>
+    </TabPanel>
+  )
+}
+
 export function ReferencePanel({
   textReference,
   bibTexReference,
 }: ReferenceProps) {
-  const [value, setValue] = useState<number>(0);
-  const [reference, setReference] = useState<string | undefined>(undefined);
+  const [value, setValue] = React.useState<string>("0")
+  const [reference, setReference] = useState<Reference>({ text: textReference });
 
-  const handleChange = (e: SyntheticEvent, newValue: number) => {
-    e.preventDefault();
-
-    setValue(newValue);
-
-    console.log(value);
-
-    if (value === 0) {
-      setReference(textReference);
-    } else {
-      setReference(bibTexReference);
-    }
+  const handleChange = (_e: SyntheticEvent, type: string) => {
+    setValue(type)
+    setReference({ text: type === "0" ? textReference : bibTexReference })
   };
 
   const copyToClipboard = () => {
-    if (reference === undefined && copy(bibTexReference)) {
-      toast.success("Copied to Clipboard");
-    }
-
-    if (reference !== undefined && copy(reference)) {
+    if (copy(reference.text)) {
       toast.success("Copied to Clipboard");
     }
   };
@@ -53,43 +75,20 @@ export function ReferencePanel({
   return (
     <Grid container>
       <Grid item xs={12} sm>
-        <Grid item xs={12}>
+        <TabContext value={value}>
           <Grid container>
             <Grid item xs={12} sx={{ borderBottom: 1, borderColor: "divider" }}>
-              <Tabs value={value} onChange={handleChange} aria-label="References">
-                <Tab sx = {{ fontSize, maxWidth: { xs: '200px', md: '400px' } }} label="bibliographic information" {...a11yProps(0)} />
-                <Tab sx={{ fontSize }} label="BibTex" {...a11yProps(1)} />
-              </Tabs>
+              <TabList onChange={handleChange} aria-label="References">
+                <Tab value="0" sx={{ fontSize, maxWidth: { xs: '200px', md: '400px' } }} label="bibliographic information" />
+                <Tab value="1" sx={{ fontSize }} label="BibTex" />
+              </TabList>
             </Grid>
             <Grid item xs={12}>
-              <CustomTabPanel value={value} index={0}>
-                {reference}
-              </CustomTabPanel>
-              <CustomTabPanel value={value} index={1}>
-                {reference === undefined ? bibTexReference : reference}
-              </CustomTabPanel>
+              {tab("0", textReference, copyToClipboard)}
+              {tab("1", bibTexReference, copyToClipboard)}
             </Grid>
           </Grid>
-        </Grid>
-      </Grid>
-      <Grid item display={'flex'} sx={{
-        alignItems: 'start',
-        justifyContent: { xs: 'center', sm: 'right' },
-        width: { xs: '100%', sm: '200px', md: '300px' }
-      }}>
-        <Button
-          sx={{
-            fontSize,
-            ...buttonPadding,
-            margin: { xs: "1rem" },
-          }}
-          variant="contained"
-          size="large"
-          disableElevation
-          onClick={copyToClipboard}
-        >
-          copy reference
-        </Button>
+        </TabContext>
       </Grid>
     </Grid>
   );
--- front/src/models/TabPanelProps.ts
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from "react";
-
-export interface TabPanelProps {
-  children?: ReactNode;
-  index: number;
-  value: number;
-}
