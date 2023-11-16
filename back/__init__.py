3bd0d035b99b938c9f03d8564b8b1e1ca56e2bfb
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-11-16 18:48:23 +0300

    fix: env class

--- back/__init__.py
+++ back/__init__.py
@@ -27,6 +27,10 @@ class Env:
     PORT: int
     
     LOG_CONFIG_PATH: str
+    
+    DO_RELOAD: bool
+    
+    ENV: str
 
     def __post_init__(self):
         self.ENABLE_AUTH = self.ENABLE_AUTH == "true"
@@ -34,6 +38,7 @@ class Env:
         self.DO_EXTRACT_COVERS = self.DO_EXTRACT_COVERS == "true"
         self.PORT = int(self.PORT)
         self.URL = f"http://{self.HOST}:{self.PORT}{self.PREFIX}"
+        self.DO_RELOAD = self.ENV == "dev"
 
 def load_dotenv(cls, path):
     # https://github.com/theskumar/python-dotenv#other-use-cases
