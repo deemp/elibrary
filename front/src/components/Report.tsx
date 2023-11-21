8fba4652a2621bd67eaa09e74cff6d4e26e2f953
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-11-21 03:05:13 +0300

    Add report page

---
+++ front/src/components/Report.tsx
@@ -0,0 +1,104 @@
+import { useEffect, useState } from "react";
+import { Book } from "../models/book";
+import { Base } from "./Base";
+import { searchLink } from "./SearchLink";
+import { Container, Grid, Typography } from "@mui/material";
+import { useFAQ } from "./FAQ";
+import * as appbar from "./AppBar";
+import { BookTable } from "./Table";
+import { Row } from "./Row";
+
+interface ReportGetReponse {
+  total_reads: number;
+  books: Book[];
+}
+
+export const Report = () => {
+  const [books, setBooks] = useState<Book[]>([]);
+  const { faqButton, faqDrawer } = useFAQ();
+  const [totalReads, setTotalReads] = useState<number>(0);
+  const [booksLoaded, setBooksLoaded] = useState<boolean>(false);
+  const url = `${import.meta.env.VITE_API_PREFIX}/report`;
+
+  useEffect(() => {
+    fetch(url, {
+      method: "GET",
+      headers: new Headers({ "content-type": "application/json" }),
+    })
+      .then((r) => r.json())
+      .then((r: ReportGetReponse) => {
+        setBooks(r.books);
+        setTotalReads(r.total_reads);
+        setBooksLoaded(true);
+      });
+  }, [url]);
+
+  return (
+    <>
+      <Base
+        title="Report"
+        user={{ isAuthenticated: true }}
+        content={
+          <Container maxWidth={"xl"}>
+            <Grid
+              container
+              rowSpacing={2}
+              marginTop={appbar.height}
+              height={"100%"}
+            >
+              <Grid marginTop={"1rem"} width={"100%"}>
+                <Row
+                  title="Total number of reads: "
+                  content={`${totalReads}`}
+                  title_width="30%"
+                  sx={{
+                    fontSize: { xs: "1.5rem", sm: "2rem" },
+                    display: "flex",
+                  }}
+                />
+                <Typography
+                  marginY={"1rem"}
+                  sx={{
+                    fontSize: { xs: "2rem", sm: "3rem" },
+                    display: "flex",
+                    justifyContent: "center",
+                  }}
+                  fontWeight={"bold"}
+                >
+                  Top 10 Books
+                </Typography>
+              </Grid>
+              <Grid item xs={12} height={"80vh"}>
+                <BookTable books={books} booksLoaded={booksLoaded} />
+              </Grid>
+            </Grid>
+          </Container>
+        }
+        nav={
+          <>
+            <Container maxWidth={"xl"}>
+              <Grid container>
+                <Grid item xs={5}>
+                  <Grid container columnSpacing={1}>
+                    <Grid item>{searchLink}</Grid>
+                    <Grid item>{faqButton}</Grid>
+                  </Grid>
+                </Grid>
+                <Grid
+                  item
+                  xs={7}
+                  display={"flex"}
+                  justifyContent={"end"}
+                  alignItems={"center"}
+                >
+                  {appbar.Ebsco}
+                </Grid>
+              </Grid>
+            </Container>
+            {faqDrawer}
+          </>
+        }
+      />
+    </>
+  );
+};
