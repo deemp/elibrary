835dd97dbfa3e3fcc5bf318b11df1e13b66337b0
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-11-26 00:11:38 +0300

    fix: book model

--- back/internal/models.py
+++ back/internal/models.py
@@ -1,50 +1,44 @@
+from datetime import datetime
 from typing import Optional
+from pydantic import BaseModel
+from sqlmodel import Field, SQLModel
+from collections import defaultdict
+from .. import env
+from sqlalchemy import JSON, Column
 from sqlmodel import Field, SQLModel
 
-from .. import env
+
+class BookSchema(BaseModel):
+    bisac: str
+    lc: str
+    publisher: str
+    year: int
+    book_id: int = Field(primary_key=True)
+    authors: Optional[str]
+    title: str
+    imprint_publisher: str
+    isbn: Optional[int]
+    esbn: int
+    oclc: Optional[int]
+    lcc: Optional[str]
+    dewey: Optional[float]
+    format: str
+    pages: int
+    read_count: defaultdict[str, int] = Field(
+        sa_column=Column(JSON), default_factory=defaultdict[str, int], nullable=False
+    )
 
 
-class Book(SQLModel, table=True):
+class Book(SQLModel, BookSchema, table=True):
     __tablename__ = env.DB_TABLE_BOOK
-    bisac: str
-    lc: str
-    publisher: str
-    year: int
-    book_id: int = Field(primary_key=True)
-    authors: Optional[str]
-    title: str
-    imprint_publisher: str
-    isbn: Optional[int]
-    esbn: int
-    oclc: Optional[int]
-    lcc: Optional[str]
-    dewey: Optional[float]
-    format: str
-    pages: int
 
 
-class BookTmp(SQLModel, table=True):
+class BookTmp(SQLModel, BookSchema, table=True):
     __tablename__ = "book_tmp"
-    bisac: str
-    lc: str
-    publisher: str
-    year: int
-    book_id: int = Field(primary_key=True)
-    authors: Optional[str]
-    title: str
-    imprint_publisher: str
-    isbn: Optional[int]
-    esbn: int
-    oclc: Optional[int]
-    lcc: Optional[str]
-    dewey: Optional[float]
-    format: str
-    pages: int
 
 
-class ReadCount(SQLModel, table=True):
-    __tablename__ = "read_count"
-    id: int = Field(primary_key=True)
-    book_id: int = Field(foreign_key="{}.book_id".format(env.DB_TABLE_BOOK))
-    month: int
-    year: int
+def read_count_key(datetime: datetime):
+    return datetime.strftime("%Y-%m")
+
+def parse_read_count_key(key):
+    return datetime.strptime(key, "%Y-%m")
