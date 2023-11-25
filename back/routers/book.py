a2f87791ae04e49365a5954cc65b8fec9c9d37ad
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-11-26 00:12:33 +0300

    fix: upd report for new book model

--- back/routers/book.py
+++ back/routers/book.py
@@ -1,8 +1,9 @@
+from collections import defaultdict
 from datetime import datetime
 from fastapi import APIRouter, HTTPException, Request
-from sqlmodel import Session
+from sqlmodel import Session, select
 from ..internal.range_request import range_requests_response
-from ..internal.models import Book, ReadCount
+from ..internal.models import Book, read_count_key
 from ..internal.db import engine
 from .. import env
 from ..internal.check import MaybeRedirect
@@ -28,10 +29,11 @@ async def file(book_id: int, request: Request, response: MaybeRedirect):
 
     with Session(engine) as session:
         if request.headers.get("range") is None:
-            read_count = ReadCount(
-                book_id=book_id, month=datetime.now().month, year=datetime.now().year
-            )
-            session.add(read_count)
+            book: Book = session.exec(select(Book).where(Book.book_id == book_id)).one()
+            time = datetime.utcnow()
+            book.read_count = defaultdict(int, book.read_count)
+            book.read_count[read_count_key(time)] += 1
+            session.add(book)
             session.commit()
 
     return range_requests_response(
--- front/src/components/Report.tsx
+++ front/src/components/Report.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Base } from "./Base";
 import { searchLink } from "./SearchLink";
 import { Box, Container, Grid, Typography } from "@mui/material";
@@ -8,15 +8,20 @@ import { AppBar } from "./AppBar";
 import { BookTable } from "./Table";
 import { bookPretty } from "../models/book";
 import { ColumnDef, ColumnHelper } from "@tanstack/react-table";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs, { Dayjs } from "dayjs";
 
 interface Book {
-  book_id: number
-  title: string
-  read_count: number
+  book_id: number;
+  title: string;
+  read_count: number;
 }
 
-interface ReportGetReponse {
-  total_reads: number;
+interface ReportPOSTReponse {
+  total_reads_month: number;
+  total_reads_year: number;
   books: Book[];
 }
 
@@ -24,100 +29,134 @@ interface ReportGetReponse {
 const columnPretty = new Map([
   ...bookPretty,
   // @ts-ignore
-  ...(new Map([['read_count', 'Read count']]))
-])
+  ...new Map([["read_count", "Read count"]]),
+]);
 
 const columns = (columnHelper: ColumnHelper<Book>) =>
   [
     {
-      id: 'book_id',
+      id: "book_id",
       size: 100,
     },
     {
-      id: 'title',
+      id: "title",
       size: 200,
     },
     {
-      id: 'read_count',
+      id: "read_count",
       size: 100,
     },
-  ].map(({ id, size }) => columnHelper.accessor(id as keyof Book, {
-    header: () => columnPretty.get(id),
-    size,
-    cell: props => props.getValue()
-  })) as ColumnDef<Book, unknown>[];
+  ].map(({ id, size }) =>
+    columnHelper.accessor(id as keyof Book, {
+      header: () => columnPretty.get(id),
+      size,
+      cell: (props) => props.getValue(),
+    })
+  ) as ColumnDef<Book, unknown>[];
 
 const url = `${import.meta.env.VITE_API_PREFIX}/report`;
 
 export const Report = () => {
-  const [books, setBooks] = useState<Book[]>([]);
   const { faqButton, faqDrawer } = useFAQ();
-  const [totalReads, setTotalReads] = useState<number>(0);
+  const [books, setBooks] = useState<Book[]>([]);
+  const [totalReadsMonth, setTotalReadsMonth] = useState<number>(0);
+  const [totalReadsYear, setTotalReadsYear] = useState<number>(0);
   const [booksLoaded, setBooksLoaded] = useState<boolean>(false);
+  const [date, setDate] = useState<Dayjs>(dayjs());
 
-  useEffect(() => {
-    fetch(url, {
-      method: "GET",
-      headers: new Headers({ "content-type": "application/json" }),
-    })
-      .then((r) => r.json())
-      .then((r: ReportGetReponse) => {
-        setBooks(r.books);
-        setTotalReads(r.total_reads);
-        setBooksLoaded(true);
-      });
-  }, []);
+  const setData = useCallback(() => {
+    if (!(date === null)) {
+      fetch(url, {
+        method: "POST",
+        headers: new Headers({ "content-type": "application/json" }),
+        body: JSON.stringify({ year: date.year(), month: date.month() + 1 }),
+      })
+        .then((r) => r.json())
+        .then((r: ReportPOSTReponse) => {
+          setBooks(r.books);
+          setTotalReadsMonth(r.total_reads_month);
+          setTotalReadsYear(r.total_reads_year);
+          setBooksLoaded(true);
+        });
+    }
+  }, [date]);
+
+  useEffect(() => setData(), [setData]);
 
   return (
-    <>
-      <Base
-        title="Report"
-        user={{ isAuthenticated: true }}
-        content={
-          <Container maxWidth={"xl"}>
+    <Base
+      title="Report"
+      user={{ isAuthenticated: true }}
+      content={
+        <Container maxWidth={"xl"}>
+          <Grid
+            container
+            rowSpacing={2}
+            columnSpacing={2}
+            marginTop={appbar.height}
+            height={`calc(100vh - ${appbar.height})`}
+          >
             <Grid
-              container
-              rowSpacing={2}
-              marginTop={appbar.height}
-              height={`calc(100vh - ${appbar.height})`}
+              item
+              xs={12}
+              md={"auto"}
+              sx={{
+                display: "flex",
+                justifyContent: { xs: "center", md: "start" },
+              }}
             >
-              <Grid item xs={12} paddingTop={"1rem"}>
-                <Typography sx={{ fontSize: { xs: "1rem", sm: "1.5rem" } }}>
-                  Total number of reads: {' '}
-                  <Box fontWeight='fontWeightMedium' display='inline'>
-                    {totalReads}
-                  </Box>
-                </Typography>
-              </Grid>
-              <Grid item xs={12}>
-                <Typography
-                  marginY={"1rem"}
-                  sx={{
-                    fontSize: { xs: "1.5rem", sm: "2rem" },
-                    display: "flex",
-                    justifyContent: "center",
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  minDate={dayjs("2023-11")}
+                  maxDate={dayjs("2042-11")}
+                  defaultValue={dayjs()}
+                  views={["month", "year"]}
+                  onChange={(value) => {
+                    if (value) setDate(value);
                   }}
-                  fontWeight={"bold"}
-                >
-                  Top 10 Books
-                </Typography>
-              </Grid>
-              <Grid item xs={12} height={"80%"}>
-                <BookTable books={books} booksLoaded={booksLoaded} columns={columns} />
-              </Grid>
+                />
+              </LocalizationProvider>
             </Grid>
-          </Container>
-        }
-        nav={
-          <AppBar
-            faqDrawer={faqDrawer}
-            leftChildren={[
-              <Grid item>{searchLink}</Grid>,
-              <Grid item>{faqButton}</Grid>,
-            ]}
-          />
-        }
-      />
-    </>
+            <Grid
+              item
+              xs={12}
+              md
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: { xs: "center", md: "start" },
+              }}
+            >
+              <Typography sx={{ fontSize: "2rem" }}>
+                Reads (month/year):{" "}
+                <Box fontWeight="fontWeightMedium" display="inline">
+                  {totalReadsMonth}
+                </Box>
+                /
+                <Box fontWeight="fontWeightMedium" display="inline">
+                  {totalReadsYear}
+                </Box>
+              </Typography>
+            </Grid>
+            <Grid item xs={12} height={{ xs: "80%", md: "90%" }}>
+              <BookTable
+                books={books}
+                booksLoaded={booksLoaded}
+                columns={columns}
+              />
+            </Grid>
+          </Grid>
+        </Container>
+      }
+      nav={
+        <AppBar
+          faqDrawer={faqDrawer}
+          leftChildren={[
+            <Grid item>{searchLink}</Grid>,
+            <Grid item>{faqButton}</Grid>,
+          ]}
+        />
+      }
+    />
   );
 };
