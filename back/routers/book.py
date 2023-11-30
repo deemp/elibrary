e0e8b3d670f25c5f506a95b23ba1d1a10b258422
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-12-01 01:15:05 +0300

    add: use utc offset env variable

--- back/routers/book.py
+++ back/routers/book.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from datetime import datetime
+from datetime import datetime, timedelta
 from fastapi import APIRouter, HTTPException, Request
 from sqlmodel import Session, select
 from ..internal.range_request import range_requests_response
@@ -30,7 +30,7 @@ async def file(book_id: int, request: Request, response: MaybeRedirect):
     with Session(engine) as session:
         if request.headers.get("range") is None:
             book: Book = session.exec(select(Book).where(Book.book_id == book_id)).one()
-            time = datetime.utcnow()
+            time = datetime.utcnow() + timedelta(hours=env.UTC_OFFSET)
             book.read_count = defaultdict(int, book.read_count)
             book.read_count[read_count_key(time)] += 1
             session.add(book)
