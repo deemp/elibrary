e93b78338309b4b59c0b06cd658f387a0a9b14b3
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-11-30 22:08:37 +0300

    fix: align text right

--- front/src/components/AppBar.tsx
+++ front/src/components/AppBar.tsx
@@ -105,6 +105,7 @@ export function AppBar({
             display={"flex"}
             justifyContent={"end"}
             alignItems={"center"}
+            textAlign={'right'}
           >
             {Ebsco}
           </Grid>
