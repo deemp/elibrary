d5f349ca25a0a9fec7af7f1f878b003aa77488e4
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-12-10 16:30:05 +0300

    fix: props type

--- front/src/components/P.tsx
+++ front/src/components/P.tsx
@@ -1,3 +1,6 @@
 import { Typography } from "@mui/material";
 
-export const P = (props) => <Typography {...props} component="p" />;
+export const P = (props: {
+  children: React.ReactNode;
+  elementType?: keyof JSX.IntrinsicElements;
+}) => <Typography {...props} component="p" />;
